e889328532c1c9b8729c4318947d95ba2dc36364
JonXia <1219133301@qq.com>
2018-12-17 10:00:17 +0800

    Initial commit

---
+++ src/main/java/com/xsj/algnowcoder/AlgNowcoderApplication.java
@@ -0,0 +1,14 @@
+package com.xsj.algnowcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgNowcoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgNowcoderApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/xsj/algnowcoder/AlgNowcoderApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xsj.algnowcoder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgNowcoderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
