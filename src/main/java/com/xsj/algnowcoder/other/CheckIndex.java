79df3c7bc30f73673a127bf3febf644fe0b57273
JonXia <1219133301@qq.com>
2018-12-18 21:35:27 +0800

    增加两道简单算法题

---
+++ src/main/java/com/xsj/algnowcoder/other/CheckIndex.java
@@ -0,0 +1,37 @@
+package com.xsj.algnowcoder.other;
+
+/**
+ * @program: alg-nowcoder
+ * @description: 递归二分查找元素的位置(not index)
+ * @author: John_Xia
+ * @create: 2018-12-18 21:12
+ */
+public class CheckIndex {
+    /**
+     * @param array 查询的数组
+     * @param start 开始位置
+     * @param end 结束位置
+     * @param find 要查找的值
+     * @return 值在数组中的位置
+     */
+    public static int search(int[] array, int start, int end, int find){
+        if(array == null){
+            return -1;
+        }
+        int middle = (start + end) / 2;//中间的位置
+        if(start <= end){
+            if(array[middle] == find){//中间值等于要查找的值，直接返回
+                return middle + 1;
+            }else if(array[middle] < find){//中间值小于要查找的值，就在中间值后面继续查找
+                return search(array, middle + 1, end, find);
+            }else{//中间值大于要查找的值，就在中间值前面继续查找
+                return search(array, start, middle -1, find);
+            }
+        }
+        return -1;//未查找到，返回-1
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{2,1,3,4,5},0,4,2));
+    }
+}
---
+++ src/main/java/com/xsj/algnowcoder/other/Nfactorial.java
@@ -0,0 +1,25 @@
+package com.xsj.algnowcoder.other;
+
+/**
+ * @program: alg-nowcoder
+ * @description: 递归求N的阶乘
+ * @author: John_Xia
+ * @create: 2018-12-18 21:12
+ */
+public class Nfactorial{
+    public static void main(String[] args){
+        int N = 5;
+        for(int n = 0; n <= N; n++){
+            int fact = factorial(n);
+            System.out.println(n + " 的阶乘值是： " + fact);
+        }
+    }
+
+    // 一个方法的方法体内调用该方法本身就是递归方法。
+    public static int factorial(int n){
+        if(n < 1) {
+            return 1;
+        }
+        return factorial(n-1) * n;
+    }
+}
