37a302758ebd622eb3b473842e5c95ceb7f2046f
JonXia <1219133301@qq.com>
2019-01-02 23:28:36 +0800

    next bigger

---
+++ src/main/java/com/xsj/algnowcoder/other/test.java
@@ -0,0 +1,69 @@
+package com.xsj.algnowcoder.other;
+
+import java.lang.reflect.Array;
+
+public class test {
+
+    public static void main(String[] args) {
+        Integer num = 1323;// 输入的数字
+        String str = num.toString();// 转化为字符串
+        int[] intArray = new int[str.length()];// 新建一个数组用来保存num每一位的数字
+        for (int i = 0; i < str.length(); i++) {// 遍历str将每一位数字添加如intArray
+            Character ch = str.charAt(i);
+            intArray[i] = Integer.parseInt(ch.toString());
+            int temp=0;
+            if(i>=str.length()-1){
+                temp=intArray[i-1];
+                intArray[i-1]=intArray[i];
+                intArray[i]=temp;
+            }
+        }
+
+
+
+        for (int i = 0; i < intArray.length; i++) {// 遍历打印int[],察看运行结果.
+            System.err.print(intArray[i] + " ");
+        }
+    }
+
+    public class MySolution {
+        public void nextPermutation(int[] ints) {
+            int i = ints.length-2;
+//            坐标移到倒数第二个
+            while ( ints[i] >= ints[i + 1] && i >= 0) {
+//                倒数第二个比第一个大
+                i--;
+            }
+
+            if (i >= 0) {
+//                如果没遍历完
+                int j = ints.length - 1;
+//                就和倒数第一的比
+                while (j >= 0 && ints[i] >= ints[j]) {
+//                倒数第一个
+                    j--;
+//                    比完，游标移动
+                }
+
+                swap(ints, i, j);
+            }
+
+            reverse(ints, i + 1);
+        }
+
+        private void reverse(int[] nums, int start) {
+            int end = nums.length - 1;
+            while (start <= end) {
+                int tmp = nums[start];
+                nums[start++] = nums[end];
+                nums[end--] = tmp;
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+}
